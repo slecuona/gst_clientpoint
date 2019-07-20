7ed24e1ea46365c6c99611efb977fe2fe195cd07
Santiago Lecuona <stournee@fpasoftware.com.ar>
2019-07-20 13:02:56 -0300

    Common proj + Espf Proj + Config

---
+++ src/Common/Config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Common {
+    public static class Config {
+
+        private static NameValueCollection AppSettings => 
+            ConfigurationManager.AppSettings;
+
+        static string GetSetting(string key, string def) =>
+            AppSettings[key] ?? def;
+
+        static Config() {
+            // ESPF
+            EspfIp = GetSetting("EspfIp", "127.0.0.1");
+            EspfPort = GetSetting("EspfPort", "18000");
+            EspfPrinter = GetSetting("EspfPrinter", "Evolis KC200");
+        }
+
+        /// <summary>
+        /// Ip del servidor ESPF
+        /// </summary>
+        public static string EspfIp;
+
+        /// <summary>
+        /// Puerto del servidor ESPF
+        /// </summary>
+        public static string EspfPort;
+
+        /// <summary>
+        /// Nombre de la impresora configurada en el ESPF
+        /// </summary>
+        public static string EspfPrinter;
+
+    }
+}
---
+++ src/Tests/ConfigFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class ConfigFixture {
+        [TestMethod]
+        public void Settings() {
+            Assert.AreEqual("127.0.0.1", Config.EspfIp);
+            Assert.AreEqual("18000", Config.EspfPort);
+            Assert.AreEqual("Evolis KC200", Config.EspfPrinter);
+        }
+    }
+}
