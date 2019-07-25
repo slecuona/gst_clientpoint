b20f97649d744cb2f48e4720f25ee154f8a97dad
Santi Lecuona <santiago.elias.lecuona@gmail.com>
2019-07-25 11:48:20 -0300

    ApiQuery.LoadAds

---
+++ src/ClientPoint/Api/ApiQuery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ClientPoint.Espf;
+
+namespace ClientPoint.Api {
+    public static class ApiQuery {
+        private static string BaseUrl = "http://192.168.10.100/mobile/api/ClientPoint/";
+
+
+        private static string Request(string op, string json) {
+            try {
+                Debug.WriteLine($"[JSON REQUEST] => {json}");
+                using (var client = new HttpClient()) {
+                    client.BaseAddress = new Uri(BaseUrl);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var result = client.PostAsync(op, content).Result;
+                    var res = result.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine($"[JSON RESPONSE] => {res}");
+                    return res;
+                }
+            }
+            catch (Exception ex) {
+                throw new Exception(
+                    $"Error al enviar request a la API. " +
+                    $"Op => {op}. Json => {json}", ex);
+            }
+        }
+
+        public static List<string> LoadAdvertising() {
+            try {
+                var json = Request("LoadAdvertising", "");
+                var res = (string[]) JsonUtils.Deserialize(typeof(string[]), json);
+                return res.ToList();
+            }
+            catch (Exception ex) {
+                throw new Exception("Error al obtener publicidades.", ex);
+            }
+        }
+    }
+}
---
+++ src/Tests/ApiFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClientPoint;
+using ClientPoint.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class ApiFixture {
+        [TestMethod]
+        public void LoadAdvertising() {
+            var res = ApiQuery.LoadAdvertising();
+            Assert.IsNotNull(res);
+        }
+    }
+}
