f2af83dd7393e13af090224c6370e98f72a7b997
Santi Lecuona <santiago.elias.lecuona@gmail.com>
2019-08-02 21:44:04 -0300

    FrmClientUpdate

---
+++ src/ClientPoint/UI/FrmClientUpdate.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ClientPoint.UI
+{
+    partial class FrmClientUpdate {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.footerPanel = new FooterPanel();
+            this.radScrollablePanel1 = new Telerik.WinControls.UI.RadScrollablePanel();
+            this.fldEmail = new ClientPoint.UI.CustomMaskedField();
+            this.fldCellphone = new ClientPoint.UI.CustomField();
+            ((System.ComponentModel.ISupportInitialize)(this.footerPanel)).BeginInit();
+            this.footerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radScrollablePanel1)).BeginInit();
+            this.radScrollablePanel1.PanelContainer.SuspendLayout();
+            this.radScrollablePanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radScrollablePanel1
+            // 
+            this.radScrollablePanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radScrollablePanel1.AutoScrollMargin = new System.Drawing.Size(0, 50);
+            this.radScrollablePanel1.AutoScrollMinSize = new System.Drawing.Size(0, 50);
+            this.radScrollablePanel1.Location = new System.Drawing.Point(85, 116);
+            this.radScrollablePanel1.Name = "radScrollablePanel1";
+            this.radScrollablePanel1.Padding = new System.Windows.Forms.Padding(0);
+            // 
+            // radScrollablePanel1.PanelContainer
+            // 
+            this.radScrollablePanel1.PanelContainer.AutoScrollMargin = new System.Drawing.Size(0, 50);
+            this.radScrollablePanel1.PanelContainer.AutoScrollMinSize = new System.Drawing.Size(0, 50);
+            this.radScrollablePanel1.PanelContainer.Controls.Add(this.fldEmail);
+            this.radScrollablePanel1.PanelContainer.Controls.Add(this.fldCellphone);
+            this.radScrollablePanel1.PanelContainer.Location = new System.Drawing.Point(0, 0);
+            this.radScrollablePanel1.PanelContainer.Size = new System.Drawing.Size(1335, 443);
+            this.radScrollablePanel1.Size = new System.Drawing.Size(1335, 443);
+            this.radScrollablePanel1.TabIndex = 4;
+            ((Telerik.WinControls.Primitives.BorderPrimitive)(this.radScrollablePanel1.GetChildAt(0).GetChildAt(1))).Width = 0F;
+            
+            // 
+            // fldEmail
+            // 
+            this.fldEmail.CustomMaskType = ClientPoint.UI.CustomMaskType.Email;
+            this.fldEmail.Label = "Correo Electrónico";
+            this.fldEmail.Location = new System.Drawing.Point(680, 0);
+            this.fldEmail.Name = "fldEmail";
+            this.fldEmail.Size = new System.Drawing.Size(650, 53);
+            this.fldEmail.TabIndex = 3;
+            // 
+            // fldCellphone
+            // 
+            this.fldCellphone.Label = "Nr. Celular";
+            this.fldCellphone.Location = new System.Drawing.Point(680, 90);
+            this.fldCellphone.Name = "fldCellphone";
+            this.fldCellphone.Size = new System.Drawing.Size(615, 53);
+            this.fldCellphone.TabIndex = 2;
+            // 
+            // FrmAddUsr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1484, 660);
+            this.Controls.Add(this.radScrollablePanel1);
+            this.Controls.Add(this.footerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmClientUpdate";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.footerPanel)).EndInit();
+            this.footerPanel.ResumeLayout(false);
+            this.radScrollablePanel1.PanelContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radScrollablePanel1)).EndInit();
+            this.radScrollablePanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #region Windows Form Designer generated code
+
+        #endregion
+
+        private FooterPanel footerPanel;
+        private Telerik.WinControls.UI.RadScrollablePanel radScrollablePanel1;
+        private CustomMaskedField fldEmail;
+        private CustomField fldCellphone;
+    }
+}
---
+++ src/ClientPoint/UI/FrmClientUpdate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using ClientPoint.Api;
+using ClientPoint.Utils;
+
+namespace ClientPoint.UI {
+    public partial class FrmClientUpdate : FrmBase {
+        public FrmClientUpdate() {
+            InitializeComponent();
+
+            footerPanel.OnNextClick(OnNext);
+            footerPanel.OnBackClick(OnBack);
+            footerPanel.NextText = "Siguiente";
+            footerPanel.BackText = "Cancelar";
+        }
+
+        private string EmailValue => fldEmail.Value;
+        private string CellphoneValue => fldCellphone.Value;
+        
+        private bool ValidateFields() {
+            var errors = new List<string>();
+
+            if (string.IsNullOrEmpty(EmailValue) && string.IsNullOrEmpty(CellphoneValue))
+                errors.Add("Debe ingresar una dirección de correo electrónico o un número de celular.");
+
+            if (errors.Count > 0) {
+                MsgBox.Error(this, string.Join("\n", errors));
+                return false;
+            }
+            return true;
+        }
+
+        private ClientCreateRequest CreateRequest => 
+            new ClientCreateRequest() {
+                CelPhone = CellphoneValue,
+                Email = EmailValue,
+            };
+
+        private void OnNext(object sender, EventArgs e) {
+            if (!ValidateFields())
+                return;
+            string errMsg;
+            try {
+                var sucess = ApiService.ClientCreate(CreateRequest, out errMsg);
+                if (sucess) {
+                    MsgBox.Show(this, 
+                        "Cliente creado correctamente. " +
+                        "Se ha enviado el código de confirmación.");
+                    UIManager.Show(Window.Confirm);
+                    return;
+                }
+            }
+            catch (Exception ex) {
+                //TODO: Log
+                errMsg = ex.Message;
+            }
+            MsgBox.Error(this, errMsg);
+        }
+
+        private void OnBack(object sender, EventArgs e) {
+            UIManager.Show(Window.Ads);
+        }
+
+        public override void BeforeShow() {
+            fldEmail.Value = "";
+            fldCellphone.Value = "";
+            base.BeforeShow();
+        }
+    }
+}
