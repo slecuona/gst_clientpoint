27ab9876fdf2b67be973032aaf670ddc89ab5b53
Santi Lecuona <santiago.elias.lecuona@gmail.com>
2019-08-08 10:20:19 -0300

    FrmClientUpdate

--- src/ClientPoint/UI/FrmClientUpdate.cs
+++ src/ClientPoint/UI/FrmClientUpdate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ClientPoint.Api;
+using ClientPoint.Session;
 using ClientPoint.Utils;
 
 namespace ClientPoint.UI {
@@ -17,18 +18,20 @@ namespace ClientPoint.UI {
                 errors.Add("Debe ingresar una dirección de correo electrónico o un número de celular.");
         }
         
-        private ClientCreateRequest CreateRequest => 
-            new ClientCreateRequest() {
+        private ClientUpdateRequest CreateRequest => 
+            new ClientUpdateRequest() {
+                DocumentNumber = ClientSession.CurrClient.DocumentNumber,
+                Password = ClientSession.CurrClient.Password,
                 CelPhone = CellphoneValue,
                 Email = EmailValue,
             };
 
         protected override bool PerformConfirm(out string errMsg) {
-            var sucess = ApiService.ClientCreate(CreateRequest, out errMsg);
+            var sucess = ApiService.ClientUpdate(CreateRequest, out errMsg);
             if (!sucess) return false;
 
             MsgBox.Show(this,
-                "Cliente creado correctamente. " +
+                "Datos actualizados correctamente. " +
                 "Se ha enviado el código de confirmación.");
             UIManager.Show(Window.Confirm);
             return true;
