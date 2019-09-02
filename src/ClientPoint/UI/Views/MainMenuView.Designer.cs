6928edba2b4a2132347747e229f172b34a0fdabc
Santi Lecuona <santiago.elias.lecuona@gmail.com>
2019-09-02 19:07:40 -0300

    fix main manu layout

--- src/ClientPoint/UI/Views/MainMenuView.Designer.cs
+++ src/ClientPoint/UI/Views/MainMenuView.Designer.cs
@@ -32,11 +32,11 @@ namespace ClientPoint.UI.Views
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuView));
-            this.btnUpdate = new CustomButtonBlue();
-            this.btnNewlient = new CustomButton();
-            this.btnConfirm = new CustomButtonBlue();
-            this.btnClient = new CustomButton();
-            this.lblWelcome = new CustomLabel();
+            this.btnUpdate = new ClientPoint.UI.Controls.CustomButtonBlue();
+            this.btnNewlient = new ClientPoint.UI.Controls.CustomButton();
+            this.btnConfirm = new ClientPoint.UI.Controls.CustomButtonBlue();
+            this.btnClient = new ClientPoint.UI.Controls.CustomButton();
+            this.lblWelcome = new ClientPoint.UI.Controls.CustomLabel();
             this.imgHost = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.panelContainer)).BeginInit();
             this.panelContainer.SuspendLayout();
@@ -50,13 +50,13 @@ namespace ClientPoint.UI.Views
             // 
             // panelContainer
             // 
-            this.panelContainer.Controls.Add(this.lblWelcome);
             this.panelContainer.Controls.Add(this.imgHost);
+            this.panelContainer.Controls.Add(this.lblWelcome);
             this.panelContainer.Controls.Add(this.btnClient);
             this.panelContainer.Controls.Add(this.btnConfirm);
             this.panelContainer.Controls.Add(this.btnNewlient);
             this.panelContainer.Controls.Add(this.btnUpdate);
-            this.panelContainer.Size = new System.Drawing.Size(1302, 567);
+            this.panelContainer.Size = new System.Drawing.Size(1352, 661);
             // 
             // btnUpdate
             // 
@@ -64,7 +64,7 @@ namespace ClientPoint.UI.Views
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.btnUpdate.ForeColor = System.Drawing.Color.White;
             this.btnUpdate.Image = ((System.Drawing.Image)(resources.GetObject("btnUpdate.Image")));
-            this.btnUpdate.Location = new System.Drawing.Point(744, 549);
+            this.btnUpdate.Location = new System.Drawing.Point(744, 591);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
             this.btnUpdate.Size = new System.Drawing.Size(421, 87);
@@ -79,12 +79,12 @@ namespace ClientPoint.UI.Views
             this.btnNewlient.ForeColor = System.Drawing.Color.White;
             this.btnNewlient.Image = ((System.Drawing.Image)(resources.GetObject("btnNewlient.Image")));
             this.btnNewlient.ImageAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnNewlient.Location = new System.Drawing.Point(356, 410);
+            this.btnNewlient.Location = new System.Drawing.Point(356, 452);
             this.btnNewlient.Name = "btnNewlient";
             this.btnNewlient.Size = new System.Drawing.Size(240, 89);
             this.btnNewlient.TabIndex = 6;
             this.btnNewlient.TabStop = false;
-            this.btnNewlient.Type = Type.NewClient;
+            this.btnNewlient.Type = ClientPoint.UI.Controls.Type.NewClient;
             this.btnNewlient.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnConfirm
@@ -93,7 +93,7 @@ namespace ClientPoint.UI.Views
             this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.btnConfirm.ForeColor = System.Drawing.Color.White;
             this.btnConfirm.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirm.Image")));
-            this.btnConfirm.Location = new System.Drawing.Point(175, 549);
+            this.btnConfirm.Location = new System.Drawing.Point(175, 591);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
             this.btnConfirm.Size = new System.Drawing.Size(421, 87);
@@ -107,12 +107,12 @@ namespace ClientPoint.UI.Views
             this.btnClient.ForeColor = System.Drawing.Color.White;
             this.btnClient.Image = ((System.Drawing.Image)(resources.GetObject("btnClient.Image")));
             this.btnClient.ImageAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnClient.Location = new System.Drawing.Point(744, 410);
+            this.btnClient.Location = new System.Drawing.Point(744, 452);
             this.btnClient.Name = "btnClient";
             this.btnClient.Size = new System.Drawing.Size(240, 89);
             this.btnClient.TabIndex = 7;
             this.btnClient.TabStop = false;
-            this.btnClient.Type = Type.Client;
+            this.btnClient.Type = ClientPoint.UI.Controls.Type.Client;
             // 
             // lblWelcome
             // 
@@ -124,28 +124,27 @@ namespace ClientPoint.UI.Views
             this.lblWelcome.Location = new System.Drawing.Point(0, 0);
             this.lblWelcome.Name = "lblWelcome";
             this.lblWelcome.Padding = new System.Windows.Forms.Padding(0, 80, 0, 0);
-            this.lblWelcome.Size = new System.Drawing.Size(1302, 143);
+            this.lblWelcome.Size = new System.Drawing.Size(1352, 143);
             this.lblWelcome.TabIndex = 4;
             this.lblWelcome.Text = "BIENVENIDO";
             this.lblWelcome.TextAlignment = System.Drawing.ContentAlignment.TopCenter;
-            this.lblWelcome.DoubleClick += LblWelcomeOnDoubleClick;
             // 
             // imgHost
             // 
-            this.imgHost.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.imgHost.Dock = System.Windows.Forms.DockStyle.Top;
             this.imgHost.Image = global::ClientPoint.Properties.Resources.logo_big;
-            this.imgHost.Location = new System.Drawing.Point(1289, 725);
+            this.imgHost.Location = new System.Drawing.Point(0, 143);
             this.imgHost.Name = "imgHost";
-            this.imgHost.Size = new System.Drawing.Size(1366, 251);
+            this.imgHost.Size = new System.Drawing.Size(1352, 303);
             this.imgHost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.imgHost.TabIndex = 0;
             this.imgHost.TabStop = false;
             // 
-            // PanelMainMenu
+            // MainMenuView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.Name = "MainMenuView";
-            this.Size = new System.Drawing.Size(1302, 567);
+            this.Size = new System.Drawing.Size(1352, 661);
             ((System.ComponentModel.ISupportInitialize)(this.panelContainer)).EndInit();
             this.panelContainer.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btnUpdate)).EndInit();
