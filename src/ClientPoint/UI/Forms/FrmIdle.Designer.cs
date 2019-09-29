0a3940fe7866c9051fa32ca68c6237b2947cdef0
Santi Lecuona <santiago.elias.lecuona@gmail.com>
2019-09-29 16:06:16 -0300

    FrmIdle

---
+++ src/ClientPoint/UI/Forms/FrmIdle.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System.Drawing;
+
+namespace ClientPoint.UI.Forms
+{
+    partial class FrmIdle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmIdle));
+            this.btnContinue = new ClientPoint.UI.Controls.CustomButtonWhiteRound();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.lblIcon = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.lblTime = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.btnContinue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.BackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Bold);
+            this.btnContinue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(77)))));
+            this.btnContinue.Image = ((System.Drawing.Image)(resources.GetObject("btnContinue.Image")));
+            this.btnContinue.Location = new System.Drawing.Point(324, 126);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(240, 89);
+            this.btnContinue.TabIndex = 0;
+            this.btnContinue.TabStop = false;
+            this.btnContinue.Text = "SI, CONTINUAR...";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Font = new System.Drawing.Font("Roboto", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel1.ForeColor = System.Drawing.Color.White;
+            this.radLabel1.Location = new System.Drawing.Point(132, 59);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(201, 41);
+            this.radLabel1.TabIndex = 1;
+            this.radLabel1.Text = "¿SIGUES AHÍ?";
+            // 
+            // lblIcon
+            // 
+            this.lblIcon.Image = global::ClientPoint.Properties.Resources.idle;
+            this.lblIcon.Location = new System.Drawing.Point(18, 18);
+            this.lblIcon.Name = "lblIcon";
+            this.lblIcon.Size = new System.Drawing.Size(82, 82);
+            this.lblIcon.TabIndex = 2;
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.ForeColor = System.Drawing.Color.White;
+            this.radLabel2.Location = new System.Drawing.Point(132, 18);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(415, 34);
+            this.radLabel2.TabIndex = 3;
+            this.radLabel2.Text = "HEMOS DETECTADO INACTIVIDAD...";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = false;
+            this.lblTime.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(18, 106);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(82, 34);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "10";
+            this.lblTime.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmIdle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(77)))));
+            this.ClientSize = new System.Drawing.Size(577, 227);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.lblIcon);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.btnContinue);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmIdle";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inactividad";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.btnContinue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.CustomButtonWhiteRound btnContinue;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel lblIcon;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel lblTime;
+    }
+}
---
+++ src/ClientPoint/UI/Forms/FrmIdle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace ClientPoint.UI.Forms
+{
+    public partial class FrmIdle : Telerik.WinControls.UI.RadForm
+    {
+        public FrmIdle()
+        {
+            InitializeComponent();
+        }
+    }
+}
